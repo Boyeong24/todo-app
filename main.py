9141fe4cf61fed19c847f92753a8df4719534e7a
Boyeong24 <qkrqhdudpk0110@gmail.com>
2026-08-07 15:48:16 +0900

    Edit tasks

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def main():
         print("1. Add Task")
         print("2. Show Tasks")
         print("3. Delete Task")
-        print("4. Exit")
+        print("4. Edit Task")
+        print("5. Exit")
 
         choice=input("Choose: ")
 
@@ -31,8 +32,20 @@ def main():
                 delete_num=int(input("Delete number: "))
                 tasks.pop(delete_num-1)
                 print("Task deleted!")
-                
+
         elif choice=="4":
+            if len(tasks)==0:
+               print("No tasks yet")
+            else:
+                edit_num=int(input("Edit number: "))
+                if 1<=edit_num<=len(tasks):
+                    new_task=input("New task: ")
+                    tasks[edit_num-1]=new_task
+                    print("Task edited!")
+                else:
+                    print("Invalid task number.")
+                
+        elif choice=="5":
             print("Goodbye!")
             break
 
